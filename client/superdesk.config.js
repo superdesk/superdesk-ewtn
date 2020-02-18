04ef1105b27062392747f9315f9f580930d9b81c
Paweł Mikołajczuk <pawel@mikolajczuk.in>
2020-02-18 12:04:57 +0100

    initial commit from superdesk/superdesk#publisher-2.0#036331e65058bc7a9c39081bdb5580085c0cd513

---
+++ client/superdesk.config.js
@@ -0,0 +1,72 @@
+/**
+ * This is the default configuration file for the Superdesk application. By default,
+ * the app will use the file with the name "superdesk.config.js" found in the current
+ * working directory, but other files may also be specified using relative paths with
+ * the SUPERDESK_CONFIG environment variable or the grunt --config flag.
+ */
+module.exports = function(grunt) {
+    return {
+        apps: [
+            'superdesk-publisher'
+        ],
+        importApps: [
+            'superdesk-publisher'
+        ],
+        defaultRoute: '/workspace/personal',
+        validatorMediaMetadata: {
+            headline: {
+                required: true
+            },
+            alt_text: {
+                required: true
+            },
+            description_text: {
+                required: true
+            },
+            copyrightholder: {
+                required: false
+            },
+            byline: {
+                required: false
+            },
+            usageterms: {
+                required: false
+            },
+            copyrightnotice: {
+                required: false
+            }
+        },
+
+        publisher: {
+            protocol: 'https',
+            tenant: process.env.PUBLISHER_API_SUBDOMAIN || '',
+            domain: process.env.PUBLISHER_API_DOMAIN || 'localhost',
+            base: 'api/v2',
+            wsDomain: process.env.PUBLISHER_WS_DOMAIN || process.env.PUBLISHER_API_DOMAIN,
+            wsPath: process.env.PUBLISHER_WS_PATH || '',
+            wsPort: process.env.PUBLISHER_WS_PORT || '8080'
+        },
+
+        langOverride: {
+            'en': {
+                'ANPA Category': 'Category',
+                'ANPA CATEGORY': 'CATEGORY'
+            }
+        },
+
+        features: {
+            preview: 1,
+            swimlane: {columnsLimit: 4},
+            editor3: true,
+            editorHighlights: true,
+            nestedItemsInOutputStage: true,
+        },
+        workspace: {
+            analytics: true
+        },
+        
+        raven: {
+            dsn: process.env.SUPERDESK_RAVEN_DSN || ''
+        }
+    };
+};
---
+++ server/settings.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013, 2014, 2015 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+import os
+from pathlib import Path
+
+
+def env(variable, fallback_value=None):
+    env_value = os.environ.get(variable, '')
+    if len(env_value) == 0:
+        return fallback_value
+    else:
+        if env_value == "__EMPTY__":
+            return ''
+        else:
+            return env_value
+
+
+ABS_PATH = str(Path(__file__).resolve().parent)
+
+init_data = Path(ABS_PATH) / 'data'
+if init_data.exists():
+    INIT_DATA_PATH = init_data
+
+RENDITIONS = {
+    'picture': {
+        'thumbnail': {'width': 220, 'height': 120},
+        'viewImage': {'width': 640, 'height': 640},
+        'baseImage': {'width': 1400, 'height': 1400},
+    },
+    'avatar': {
+        'thumbnail': {'width': 60, 'height': 60},
+        'viewImage': {'width': 200, 'height': 200},
+    }
+}
+
+WS_HOST = env('WSHOST', '0.0.0.0')
+WS_PORT = env('WSPORT', '5100')
+
+LOG_CONFIG_FILE = env('LOG_CONFIG_FILE', 'logging_config.yml')
+
+REDIS_URL = env('REDIS_URL', 'redis://localhost:6379')
+if env('REDIS_PORT'):
+    REDIS_URL = env('REDIS_PORT').replace('tcp:', 'redis:')
+BROKER_URL = env('CELERY_BROKER_URL', REDIS_URL)
+
+SECRET_KEY = env('SECRET_KEY', '')
+PUBLISH_ASSOCIATED_ITEMS = True
