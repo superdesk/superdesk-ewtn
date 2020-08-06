bb5541c6ec2c98a4301ca41b32546e1a2f45e1b8
Petr <petr.jasek@sourcefabric.org>
2020-08-06 16:03:18 +0200

    implement fix_timestamps command
    it will set `versioncreated` to `extra.original_published_at` value
    
    SDESK-5429

--- server/manage.py
+++ server/manage.py
@@ -11,12 +11,36 @@
 
 """Superdesk Manager"""
 
+import arrow
 import superdesk
-from flask.ext.script import Manager
+
+from flask_script import Manager
 from app import get_app
 
 app = get_app()
 manager = Manager(app)
+
+
+@manager.option('-l', '--limit', type=int)
+def fix_timestamps(limit=0):
+    lookup = {'extra.original_published_at': {'$ne': None}}
+    items = superdesk.get_resource_service('archive').get_from_mongo(req=None, lookup=lookup)
+    updated = 0
+    for item in items:
+        try:
+            published = arrow.get(item['extra']['original_published_at']).datetime
+        except ValueError:
+            continue
+        updates = {'versioncreated': published}
+        print('updating item {} published at {}'.format(item['_id'], published))
+        superdesk.get_resource_service('archive').system_update(item['_id'], updates, item)
+        superdesk.get_resource_service('published').update_published_items(item['_id'], 'versioncreated',
+                                                                           updates['versioncreated'])
+        updated += 1
+        if limit and updated >= limit:
+            break
+    print('done updating {} items'.format(updated))
+
 
 if __name__ == '__main__':
     manager.run(superdesk.COMMANDS)
